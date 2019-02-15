fbf43e7292cdd25e2ec3f330fa2e093eda6bca40
Neil Haggerty <neil.haggerty@mergermarket.com>
2019-02-15 10:54:31 +0000

    Adds secrets branch for container defs

--- main.tf
+++ main.tf
@@ -35,7 +35,7 @@ module "taskdef" {
 }
 
 module "service_container_definition" {
-  source = "github.com/mergermarket/tf_ecs_container_definition"
+  source = "github.com/mergermarket/tf_ecs_container_definition?ref=secrets"
 
   name               = "${lookup(var.release, "component")}${var.name_suffix}"
   image              = "${lookup(var.release, "image_id")}"
